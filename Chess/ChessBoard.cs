c184710b49afd790385d2ca72c09ed06898dfc85
dimitry-lzs <lazanasdimitrios@gmail.com>
2024-02-04 06:14:01 +0200

    Add figures

--- Chess/ChessBoard.cs
+++ Chess/ChessBoard.cs
@@ -18,18 +18,18 @@ namespace Chess
         public ChessBoard()
         {
             InitializeComponent();
-            this.fillGrid();
+            this.DoubleBuffered = true;
+            this.FillGrid();
         }
 
-        private void fillGrid()
+        private void FillGrid()
         {
             for (int i = 0; i < BOARD_SIZE; i++)
             {
                 for (int j = 0; j < BOARD_SIZE; j++)
                 {
-                    PictureBox square = new PictureBox();
+                    Panel square = new Panel();
                     square.BackColor = (i + j) % 2 == 0 ? Color.Moccasin : Color.FromArgb(101, 67, 33);
-                    ;
                     square.Location = new Point(i * SQUARE_SIZE, j * SQUARE_SIZE);
                     square.Size = new Size(SQUARE_SIZE, SQUARE_SIZE);
                     this.Controls.Add(square);
@@ -37,11 +37,91 @@ namespace Chess
             }
         }
 
+        private void PlaceFigures()
+        {
+            for (int i = 0; i < BOARD_SIZE; i++)
+            {
+                for (int j = 0; j < BOARD_SIZE; j++)
+                {
+                    PictureBox piece = new PictureBox();
+
+                    if (j == 0)
+                    {
+                        switch (i)
+                        {
+                            case 0:
+                                piece.Image = Image.FromFile("Chess_rlt60.png");
+                                break;
+                            case 1:
+                                piece.Image = Image.FromFile("Chess_nlt60.png");
+                                break;
+                            case 2:
+                                piece.Image = Image.FromFile("Chess_blt60.png");
+                                break;
+                            case 3:
+                                piece.Image = Image.FromFile("Chess_klt60.png");
+                                break;
+                            case 4:
+                                piece.Image = Image.FromFile("Chess_qlt60.png");
+                                break;
+                            case 5:
+                                piece.Image = Image.FromFile("Chess_blt60.png");
+                                break;
+                            case 6:
+                                piece.Image = Image.FromFile("Chess_nlt60.png");
+                                break;
+                            case 7:
+                                piece.Image = Image.FromFile("Chess_rlt60.png");
+                                break;
+                        }
+                    } else if (j == 1)
+                    {
+                        piece.Image = Image.FromFile("Chess_plt60.png");
+                    } else if (j == 6)
+                    {
+                        piece.Image = Image.FromFile("Chess_pdt60.png");
+                    } else if (j == 7)
+                    {
+                        switch (i)
+                        {
+                            case 0:
+                                piece.Image = Image.FromFile("Chess_rdt60.png");
+                                break;
+                            case 1:
+                                piece.Image = Image.FromFile("Chess_ndt60.png");
+                                break;
+                            case 2:
+                                piece.Image = Image.FromFile("Chess_bdt60.png");
+                                break;
+                            case 3:
+                                piece.Image = Image.FromFile("Chess_kdt60.png");
+                                break;
+                            case 4:
+                                piece.Image = Image.FromFile("Chess_qdt60.png");
+                                break;
+                            case 5:
+                                piece.Image = Image.FromFile("Chess_bdt60.png");
+                                break;
+                            case 6:
+                                piece.Image = Image.FromFile("Chess_ndt60.png");
+                                break;
+                            case 7:
+                                piece.Image = Image.FromFile("Chess_rdt60.png");
+                                break;
+                        }
+                    }   
+
+                    piece.Cursor = Cursors.Hand;
+                    this.Controls[i * BOARD_SIZE + j].Controls.Add(piece);
+                }
+            }
+        }   
 
         private void ChessBoard_Load(object sender, EventArgs e)
         {
             this.Size = new Size(BOARD_SIZE * SQUARE_SIZE, BOARD_SIZE * SQUARE_SIZE);
             this.Location = new Point(30, 100);
+            this.PlaceFigures();
         }
     }
 }
