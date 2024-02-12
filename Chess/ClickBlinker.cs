de42aef2ca0cb680918204595305d7d59e334ea1
dimitry-lzs <lazanasdimitrios@gmail.com>
2024-02-12 09:33:31 +0200

    add blinker

---
+++ Chess/ClickBlinker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chess
+{
+    public class ClickBlinker : Panel
+    {
+        private int _X = 13;
+        private int _Y = 25;
+
+        System.Drawing.Image BlinkerImage;
+        private System.Drawing.Image[] blinkingImages = new System.Drawing.Image[2];
+
+        Timer timer;
+        public ClickBlinker()
+        {
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.ResizeRedraw |
+                               ControlStyles.OptimizedDoubleBuffer |
+                                              ControlStyles.AllPaintingInWmPaint, true);
+            this.BackColor = System.Drawing.Color.Transparent;
+
+            this.blinkingImages[0] = Properties.Resources.center_bulb1;
+            this.blinkingImages[1] = Properties.Resources.center_bulb2;
+            this.BlinkerImage = this.blinkingImages[0];
+
+            // For testing, normally clock controls it
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 500; // Timer will tick every 500 milliseconds (1/2 second)
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start(); // Start the timer
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            this.BlinkerImage = this.blinkingImages[0];
+            this.blinkingImages[0] = this.blinkingImages[1];
+            this.blinkingImages[1] = this.BlinkerImage;
+            this.Refresh();
+        }
+        
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(this.BlinkerImage, 0, 0, this._X, this._Y);
+        }
+    }
+}
--- Chess/Clock.Designer.cs
+++ Chess/Clock.Designer.cs
@@ -28,19 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.digit1 = new Chess.Digit();
-            this.digit2 = new Chess.Digit();
-            this.digit3 = new Chess.Digit();
             this.digit4 = new Chess.Digit();
+            this.digit3 = new Chess.Digit();
+            this.digit2 = new Chess.Digit();
+            this.digit1 = new Chess.Digit();
+            this.clickBlinker1 = new Chess.ClickBlinker();
             this.SuspendLayout();
             // 
-            // digit1
+            // digit4
             // 
-            this.digit1.BackColor = System.Drawing.Color.Transparent;
-            this.digit1.Location = new System.Drawing.Point(100, 141);
-            this.digit1.Name = "digit1";
-            this.digit1.Size = new System.Drawing.Size(43, 81);
-            this.digit1.TabIndex = 0;
+            this.digit4.BackColor = System.Drawing.Color.Transparent;
+            this.digit4.Location = new System.Drawing.Point(266, 141);
+            this.digit4.Name = "digit4";
+            this.digit4.Size = new System.Drawing.Size(43, 81);
+            this.digit4.TabIndex = 1;
+            // 
+            // digit3
+            // 
+            this.digit3.BackColor = System.Drawing.Color.Transparent;
+            this.digit3.Location = new System.Drawing.Point(217, 141);
+            this.digit3.Name = "digit3";
+            this.digit3.Size = new System.Drawing.Size(43, 81);
+            this.digit3.TabIndex = 2;
             // 
             // digit2
             // 
@@ -50,27 +59,28 @@
             this.digit2.Size = new System.Drawing.Size(43, 81);
             this.digit2.TabIndex = 1;
             // 
-            // digit3
+            // digit1
             // 
-            this.digit3.BackColor = System.Drawing.Color.Transparent;
-            this.digit3.Location = new System.Drawing.Point(247, 141);
-            this.digit3.Name = "digit3";
-            this.digit3.Size = new System.Drawing.Size(43, 81);
-            this.digit3.TabIndex = 2;
+            this.digit1.BackColor = System.Drawing.Color.Transparent;
+            this.digit1.Location = new System.Drawing.Point(100, 141);
+            this.digit1.Name = "digit1";
+            this.digit1.Size = new System.Drawing.Size(43, 81);
+            this.digit1.TabIndex = 0;
             // 
-            // digit4
+            // clickBlinker1
             // 
-            this.digit4.BackColor = System.Drawing.Color.Transparent;
-            this.digit4.Location = new System.Drawing.Point(296, 141);
-            this.digit4.Name = "digit4";
-            this.digit4.Size = new System.Drawing.Size(43, 81);
-            this.digit4.TabIndex = 1;
+            this.clickBlinker1.BackColor = System.Drawing.Color.Transparent;
+            this.clickBlinker1.Location = new System.Drawing.Point(198, 197);
+            this.clickBlinker1.Name = "clickBlinker1";
+            this.clickBlinker1.Size = new System.Drawing.Size(13, 25);
+            this.clickBlinker1.TabIndex = 3;
             // 
             // Clock
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.clickBlinker1);
             this.Controls.Add(this.digit4);
             this.Controls.Add(this.digit3);
             this.Controls.Add(this.digit2);
@@ -87,5 +97,6 @@
         private Digit digit2;
         private Digit digit3;
         private Digit digit4;
+        private ClickBlinker clickBlinker1;
     }
 }
